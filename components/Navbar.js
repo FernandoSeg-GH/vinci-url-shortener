ec6a26ea7a52994cbca11878bfcfd10b94f57f91
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-04-06 20:33:08 -0300

    updates

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="text-white fixed w-screen">
+    <header className="text-white fixed w-screen z-50">
       <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-teal-900 h-screen">
+      <main className="bg-teal-900 h-screen w-screen fixed">
         <Form />
       </main>
     </Layout>
