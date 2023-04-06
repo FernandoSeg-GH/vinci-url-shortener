397d1d130e5c525e2faf7f3e79b56a94ae65c689
Fernando Segre <77026738+FernandoSeg-GH@users.noreply.github.com>
2023-04-06 20:27:53 -0300

    analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Analytics */}
+          {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-N5KEHZ586X"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-N5KEHZ586X');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
